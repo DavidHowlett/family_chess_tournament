79321852485735c64b6e96f50026e6a8828421cc
David Howlett <david.howlett3@gmail.com>
2016-12-24 23:56:24 +0000

    game state now prints

--- David_AI.py
+++ David_AI.py
@@ -1,7 +1,7 @@
-import time
 '''This was written by David for fun on 24th December 2016
 Micheal is not allowed to read this file.
 '''
+import time
 startTime = time.perf_counter()
 
 # --------- read in game state ----------
@@ -11,10 +11,10 @@ player = gameHistory[-12][9]
 whiteTime = float(gameHistory[-11][12:])
 blackTime = float(gameHistory[-10][12:])
 if player == 'w':
-    myTime = whiteTime
+    myTime = whiteTime + 2
     theirTime = blackTime
 else:
-    myTime = blackTime
+    myTime = blackTime + 2
     theirTime = whiteTime
 gameState = gameHistory[-9:-1]
 gameState.reverse()
@@ -23,20 +23,14 @@ gameState = [[char for char in line] for line in gameState]
 # ---------- modify game state ----------
 
 # ---------- write game state -----------
-handle = open('game state.txt','a')
-handle.write('\n-------- turn: {} --------\n'.format(turn+1))
-handle.write('to move: {}\n'.format('b' if player == 'w' else 'w'))
+gameState = '\n'.join(''.join(char for char in line) for line in gameState.__reversed__())+'\n'
 moveDuration = time.perf_counter() - startTime
-handle.write('white time: {}\n'.format(whiteTime+moveDuration if player == 'w' else whiteTime))
-handle.write('black time: {}\n'.format(blackTime+moveDuration if player == 'b' else blackTime))
-handle.write('\n'.join(''.join(char for char in line) for line in gameState.__reversed__())+'\n')
-
-
-print(gameHistory)
-print(turn)
-print(player)
-print(whiteTime)
-print(blackTime)
-print(gameState)
+toWrite = '\n-------- turn: {} --------\n'.format(turn+1)
+toWrite += 'to move: {}\n'.format('b' if player == 'w' else 'w')
+toWrite += 'white time: {}\n'.format(myTime-moveDuration if player == 'w' else theirTime)
+toWrite += 'black time: {}\n'.format(myTime-moveDuration if player == 'b' else theirTime)
+toWrite += gameState
+open('game state.txt','a').write(toWrite)
+print(toWrite)
 
 
