13e95da1178e3d437bc0f78cb1cba001ecbf8a62
David Howlett <david.howlett3@gmail.com>
2016-12-25 00:25:15 +0000

    simple scoring added

--- David_AI.py
+++ David_AI.py
@@ -1,8 +1,20 @@
-'''This was written by David for fun on 24th December 2016
-Micheal is not allowed to read this file.
-'''
+"""This was written by David for fun on 24th December 2016
+Micheal is not allowed to read this file."""
 import time
 startTime = time.perf_counter()
+value = {
+    '.': 0,
+    'K': 1000, 'Q': 9, 'R': 5, 'B': 3.2, 'N': 3, 'P': 1,
+    'k': -1000, 'q': -9, 'r': -5, 'b': -3.2, 'n': -3, 'p': -1}
+
+
+def score(board:[[str]])->float:
+    'This takes a gameState object and returns the current score of white'
+    _score = 0.0
+    for row in board:
+        for square in row:
+            _score += value[square]
+    return _score
 
 # --------- read in game state ----------
 gameHistory = open('game state.txt').read().split('\n')
@@ -23,14 +35,15 @@ gameState = [[char for char in line] for line in gameState]
 # ---------- modify game state ----------
 
 # ---------- write game state -----------
-gameState = '\n'.join(''.join(char for char in line) for line in gameState.__reversed__())+'\n'
-moveDuration = time.perf_counter() - startTime
+runTime = time.perf_counter() - startTime
 toWrite = '\n-------- turn: {} --------\n'.format(turn+1)
 toWrite += 'to move: {}\n'.format('b' if player == 'w' else 'w')
-toWrite += 'white time: {}\n'.format(myTime-moveDuration if player == 'w' else theirTime)
-toWrite += 'black time: {}\n'.format(myTime-moveDuration if player == 'b' else theirTime)
-toWrite += gameState
+toWrite += 'white time: {}\n'.format(myTime-runTime if player == 'w' else theirTime)
+toWrite += 'black time: {}\n'.format(myTime-runTime if player == 'b' else theirTime)
+toWrite += '\n'.join(''.join(char for char in line) for line in gameState.__reversed__())+'\n'
 open('game state.txt','a').write(toWrite)
 print(toWrite)
+print('score: {:.3f}'.format(score(gameState)))
+print('run time: {:.5f}'.format(runTime))
 
 
