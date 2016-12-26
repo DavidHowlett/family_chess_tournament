d2b103e9ee4f05cd7a721b19229ebe8ca97cff08
David Howlett <david.howlett3@gmail.com>
2016-12-26 00:41:28 +0000

    kings and knights work

--- David_AI.py
+++ David_AI.py
@@ -8,12 +8,16 @@ PIECE_VALUE = {
     'K': 20, 'Q': 9, 'R': 5, 'B': 3.2, 'N': 3, 'P': 1,
     'k': -20, 'q': -9, 'r': -5, 'b': -3.2, 'n': -3, 'p': -1}
 PIECE_MOVE_DIRECTION = {
+    'K': ((1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)),
+    'k': ((1,0), (0,1), (-1,0), (0,-1), (1,1), (1,-1), (-1,1), (-1,-1)),
     'Q': ((1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)),
     'q': ((1,0), (0,1), (-1,0), (0,-1), (1,1), (1,-1), (-1,1), (-1,-1)),
     'R': ((1,0), (0,1), (-1,0), (0,-1)),
     'r': ((1,0), (0,1), (-1,0), (0,-1)),
     'B': ((1,1), (1,-1), (-1,1), (-1,-1)),
     'b': ((1,1), (1,-1), (-1,1), (-1,-1)),
+    'N': ((1,2), (2,1), (2,-1), (1,-2), (-1,-2), (-2,-1), (-2,1), (-1,2)),
+    'n': ((1,2), (2,1), (2,-1), (1,-2), (-1,-2), (-2,-1), (-2,1), (-1,2)),
 }
 
 
@@ -26,24 +30,16 @@ def score(board: [str])->float:
     return _score
 
 
-def move(board: [str], y1, x1, y2, x2)-> [str]:
+def move(_board: [str], y1, x1, y2, x2)-> [str]:
     """returns a board with a move made"""
-    assert 0 <= x1 <= 7
-    assert 0 <= y1 <= 7
-    assert 0 <= x2 <= 7
-    assert 0 <= y2 <= 7
-    board = board.copy()
-    piece1 = board[y1][x1]
-    piece2 = board[y2][x2]
-    assert not (piece1.isupper() and piece2.isupper())
-    assert not (piece1.islower() and piece2.islower())
+    _board = _board.copy()
     # add piece to destination
-    line = board[y2]
-    board[y2] = line[:x2] + piece1 + line[x2 + 1:]
+    line = _board[y2]
+    _board[y2] = line[:x2] + _board[y1][x1] + line[x2 + 1:]
     # remove piece from source
-    line = board[y1]
-    board[y1] = line[:x1] + '.' + line[x1 + 1:]
-    return board
+    line = _board[y1]
+    _board[y1] = line[:x1] + '.' + line[x1 + 1:]
+    return _board
 
 
 def moves(board: [str], _player: str)->[[str]]:
@@ -52,12 +48,12 @@ def moves(board: [str], _player: str)->[[str]]:
     for x in range(8):
         for y in range(8):
             piece = board[y][x]
-            if piece in 'QRB' and _player == 'w' or piece in 'qrb' and _player == 'b':
+            if piece in 'KQRBN' and _player == 'w' or piece in 'kqrbn' and _player == 'b':
                 for xd, yd in PIECE_MOVE_DIRECTION[piece]:
                     for i in range(1, 100):
                         x2 = x+i*xd
                         y2 = y+i*yd
-                        if not (0<=x2<=7 and 0<=y2<=7):
+                        if not (0 <= x2 <= 7 and 0 <= y2 <= 7):
                             # then it is a move off the board
                             break
                         target_piece = board[y2][x2]
@@ -69,7 +65,30 @@ def moves(board: [str], _player: str)->[[str]]:
                             _moves.append(move(board, y, x, y2, x2))
                             break
                         _moves.append(move(board, y, x, y2, x2))
+                        if piece in 'KkNn':
+                            break
+            '''
+            # pawns are weird
+            if piece == 'P' and _player == 'w' or piece == 'p' and _player == 'b':
+                y2 = y+1 if _player == 'w' else y-1
 
+                for i in (1,2) if (y == 1 and _player == 'w' or y == 6 and player == 'b') else (1,):
+                    y2 = y+i*yd
+                    if not 0 <= y2 <= 7:
+                        # then it is a move off the board
+                        break
+                    target_piece = board[y2][x2]
+                    if (target_piece.isupper() and _player == 'w') or (target_piece.islower() and _player == 'b'):
+                        # then it is taking it's own piece
+                        break
+                    if (target_piece.isupper() and _player == 'b') or (target_piece.islower() and _player == 'w'):
+                        # then it is taking an opponent's piece
+                        _moves.append(move(board, y, x, y2, x2))
+                        break
+                    _moves.append(move(board, y, x, y2, x2))
+                    if piece in 'KkNn':
+                        break
+            '''
     return _moves
 
 # --------- read in game state ----------
