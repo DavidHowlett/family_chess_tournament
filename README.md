e26338cb5c2b852c1090c59443bbc20c238ae6c3
David Howlett <david.howlett3@gmail.com>
2016-12-28 11:57:02 +0000

    refactor runner

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Family AI chess tournament
-Christmas 2016 I am competing with my brother and my mum to see who can produce the best chess AI.
+Christmas 2016 I am competing with my brothers
+and to see who can produce the best chess AI. 
+Mum will play the AIs we write to see if she can beat them.
 
 # Rules
 The AI will work by reading in a file called “game state.txt”
 looking at the last game state then appending a new game state to the file.
 The programs of the players will be run alternately.
-The players are required to increment their timer by 2 minus the length of time it took them to move.
 
 A player loses if:
 - They make an illegal move
--- runner.py
+++ runner.py
@@ -5,8 +5,8 @@ import subprocess
 initialTime = 5
 timePerMove = 1
 
-playerA = ['w', initialTime, 'David_AI.py']
-playerB = ['b', initialTime, 'David_AI_old.py']
+white = {'time remaining': initialTime, 'AI': 'David_AI.py'}
+black = {'time remaining': initialTime, 'AI': 'David_AI_old.py'}
 
 if os.name == 'posix':
     python = 'python3'
@@ -14,7 +14,8 @@ else:
     python = 'python'
 print(os.name)
 
-open('game state.txt', 'w').write('''-------- turn: 0 --------
+open('game state.txt', 'w').write(
+    '''-------- turn: 0 --------
 
 rnbqkbnr
 pppppppp
@@ -29,27 +30,23 @@ to move: w
 white time: {}
 black time: {}
 
-'''.format(playerA[1], playerB[1]))
+'''.format(initialTime, initialTime))
 
 for i in range(100):
-    player = playerB if i % 2 else playerA
-    colour, timeRemaining, AI = player
+    player = black if i % 2 else white
     startTime = time.perf_counter()
-    if subprocess.run((python, AI)).returncode:
-        print('{} stopped the game'.format(AI))
+    if subprocess.run((python, player['AI'])).returncode:
+        print('{} stopped the game'.format(player['AI']))
         exit()
     runTime = time.perf_counter() - startTime
-    if colour == 'w':
-        playerA[1] = timeRemaining + timePerMove - runTime
-    else:
-        playerB[1] = timeRemaining + timePerMove - runTime
+    player['time remaining'] = player['time remaining'] + timePerMove - runTime
     board = open('game state.txt').read().split('\n\n\n')[-1].split('\n\n')[-1]
     print(board)
     print('run time: {:.3f} seconds\n'.format(runTime))
     with open('game state.txt', 'a') as gameState:
-        gameState.write('\n\nto move: {}\n'.format('b' if colour == 'w' else 'w'))
-        gameState.write('white time: {:.6f}\n'.format(playerA[1]))
-        gameState.write('black time: {:.6f}\n'.format(playerB[1]))
+        gameState.write('\n\nto move: {}\n'.format('w' if i % 2 else 'b'))
+        gameState.write('white time: {:.6f}\n'.format(white['time remaining']))
+        gameState.write('black time: {:.6f}\n'.format(black['time remaining']))
         gameState.write('\n')
         if 'k' not in board:
             gameState.write('White won!\n')
