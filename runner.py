5d9b1cc97162464cf8718295473697d6afbadfa3
David Howlett <david.howlett3@gmail.com>
2017-01-22 17:17:44 +0000

    avg time taken printed
    To aid in optimisation I wanted to know how long my AI was taking in
    real world board positions. This turned out to be more complex then
    expected. I don’t really like the way I have built it.

--- runner.py
+++ runner.py
@@ -50,7 +50,8 @@ def print_state(_turn, board, run_time, white_time_remaining, black_time_remaini
 
 def match(white, black):
     """This plays a single match between the white and black players. If the match has been played before then it
-    returns quickly with the previous result"""
+    returns quickly with the previous result."""
+    # -------- memoization -------
     file_name = r'results/{} vs {}.txt'.format(
         white.__name__,
         black.__name__
@@ -64,55 +65,69 @@ def match(white, black):
         if current_versions == previous_versions:
             # then return the previous result
             return eval(file.readline())
-    except FileNotFoundError:
+    except (FileNotFoundError, SyntaxError):
         pass
+    print('Match between {} on white and {} on black'.format(white.__name__, black.__name__))
     file = open(file_name, 'w')
     file.write(current_versions)
-    print('Match between {} on white and {} on black'.format(white.__name__, black.__name__))
-    white_time_remaining = black_time_remaining = initialTime
+    # -------- turns and time -------
+    black_moves = white_moves = 0
+    black_time = white_time = 0
+    black_time_taken = white_time_taken = 0
+
     history = [[[piece for piece in line] for line in initialBoard.replace(' ', '').split()]]
     history[0].reverse()
-    to_return = 0.5, 'Draw due to reaching {} turns'.format(turnsToPlayFor)
+
+    to_return = {'score': 0.5, 'cause': 'Draw due to reaching {} turns'.format(turnsToPlayFor)}
     for turn in range(1, 1+turnsToPlayFor):
         start_time = time.process_time()
         try:
             chosen_move = (white if turn % 2 else black).main(
-                copy.deepcopy(history), white_time_remaining, black_time_remaining)
+                copy.deepcopy(history), white_time, black_time)
         except shared.StalemateException:
-            to_return = 0.5, 'Draw due to stalemate'
+            to_return = {'score': 0.5, 'cause': 'Draw due to stalemate'}
             break
         except shared.ThreeFoldRepetition:
-            to_return = 0.5, '{} called a draw with the threefold repetition rule'.format(
-                'White' if turn % 2 else 'Black')
+            to_return = {'score': 0.5, 'cause': '{} called a draw with the threefold repetition rule'.format(
+                'White' if turn % 2 else 'Black')}
             break
         except shared.FiftyMoveException:
-            to_return = 0.5, '{} called a draw with the 50 move rule'.format('White' if turn % 2 else 'Black')
+            to_return = {'score': 0.5, 'cause': '{} called a draw with the 50 move rule'.format(
+                'White' if turn % 2 else 'Black')}
             break
         run_time = time.process_time() - start_time
         history.append(chosen_move)
         if turn % 2:
-            white_time_remaining = white_time_remaining + timePerMove - run_time
+            white_moves += 1
+            white_time_taken += run_time
         else:
-            black_time_remaining = black_time_remaining + timePerMove - run_time
-        print_state(turn, chosen_move, run_time, white_time_remaining, black_time_remaining)
-        if white_time_remaining < 0:
-            to_return = 0, 'Black won due to white running out of time'
+            black_moves += 1
+            black_time_taken += run_time
+        white_time = initialTime + white_moves * timePerMove - white_time_taken
+        black_time = initialTime + black_moves * timePerMove - black_time_taken
+        print_state(turn, chosen_move, run_time, white_time, black_time)
+        if white_time < 0:
+            to_return = {'score': 0, 'cause': 'Black won due to white running out of time'}
             break
-        if black_time_remaining < 0:
-            to_return = 1, 'White won due to black running out of time'
+        if black_time < 0:
+            to_return = {'score': 1, 'cause': 'White won due to black running out of time'}
             break
         if 'P' in chosen_move[7]:
-            to_return = 0, 'Black won because white made and illegal move'
+            to_return = {'score': 0, 'cause': 'Black won because white made and illegal move'}
             break
         if 'p' in chosen_move[0]:
-            to_return = 1, 'White won because black made and illegal move'
+            to_return = {'score': 1, 'cause': 'White won because black made and illegal move'}
             break
         if not any(any(piece == 'K' for piece in row) for row in chosen_move):
-            to_return = 0, 'Black won by taking the king'
+            to_return = {'score': 0, 'cause': 'Black won by taking the king'}
             break
         if not any(any(piece == 'k' for piece in row) for row in chosen_move):
-            to_return = 1, 'White won by taking the king'
+            to_return = {'score': 1, 'cause': 'White won by taking the king'}
             break
+    to_return['white_time_taken'] = white_time_taken
+    to_return['black_time_taken'] = black_time_taken
+    to_return['white_moves'] = white_moves
+    to_return['black_moves'] = black_moves
     file.write(str(to_return))
     return to_return
 
@@ -123,30 +138,38 @@ minimise = False
 
 tournamentResults = [('white', 'black', 'result', 'explanation')]
 for player in competitors:
-    player.tournamentScore = 0
+    player.totalMoves_ = 0
+    player.totalTime_ = 0
+    player.tournamentScore_ = 0
 
 tournamentStartTime = time.perf_counter()
 for white in competitors:
     for black in competitors:
         if white == black:
             continue
-        game_start_time = time.perf_counter()
-        result, cause = match(white, black)
-        print(cause)
-        print('The game took {:.3f} seconds'.format(time.perf_counter() - game_start_time))
-        tournamentResults.append((white.__name__, black.__name__, result, cause))
-        white.tournamentScore += result
-        black.tournamentScore += (1-result)
+        result = match(white, black)
+        score = result['score']
+        white.tournamentScore_ += score
+        white.totalMoves_ += result['white_moves']
+        white.totalTime_ += result['white_time_taken']
+        black.tournamentScore_ += (1 - score)
+        black.totalMoves_ += result['black_moves']
+        black.totalTime_ += result['black_time_taken']
+
+        print(result['cause'])
+        print('The game took {:.3f} seconds'.format(result['white_time_taken'] + result['black_time_taken']))
+        tournamentResults.append((white.__name__, black.__name__, score, result['cause']))
+
 print('\nAll the matches played in the tournament are shown below')
 
 for result in tournamentResults:
     print(''.join('{:<16}'.format(r) for r in result), sep='\t')
 
-print('\nThe tournament took: {:.3f} seconds'.format(time.perf_counter()-tournamentStartTime))
+print('\nThe tournament took: {:.1f} seconds'.format(time.perf_counter()-tournamentStartTime))
 print('Each of the {} competitors has played {} games\n'.format(
     len(competitors), 2*len(competitors)-2))
 
-competitors.sort(key=lambda c: c.tournamentScore, reverse=True)
+competitors.sort(key=lambda c: c.tournamentScore_, reverse=True)
 for player in competitors:
-    print('{} score is {}/{}'.format(
-        player.__name__, player.tournamentScore, 2*len(competitors)-2))
+    print('{} scored {}/{} taking on average {:.3f} seconds'.format(
+        player.__name__, player.tournamentScore_, 2*len(competitors)-2, player.totalTime_/player.totalMoves_))
